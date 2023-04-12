3ddd7b4c8eac200a7ac02d3bb2f9082d11a6539b
Swagato Chakraborty <Swagato1024@users.noreply.github.com>
2023-04-12 13:09:05 +0530

    tests for square are passing

--- test/shape-test.js
+++ test/shape-test.js
@@ -1,5 +1,8 @@
+const test = require('../lib/testing.js');
+const shapes = require('../src/shapes/square.js');
+
 const testSquare = function() {
-  test.assertEquality("****\n****\n****\n****\n", square(4), 'should give square of given length');
+  test.assertEquality("****\n****\n****\n****\n", shapes.square(4), 'should give square of given length');
 }
 
 testSquare();
